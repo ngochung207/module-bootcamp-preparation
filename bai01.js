416ba8ac604541162b719a59b9bb21e3ee854a8d
ngochung207 <ngochung207@gmail.com>
2020-12-08 09:12:15 +0700

    bai 1 is done

---
+++ bai01.js
@@ -0,0 +1,14 @@
+// Cho danh sách các điểm thi ở trong một mảng.
+// Hãy viết hàm findMax() với đầu vào là một mảng điểm và đầu ra là điểm số cao nhất được tìm thấy trong danh sách đó.
+let arr = [4,10,40,2];
+function findMax(arr){
+    let iMax = arr[0];
+    for (let i = 0; i < arr.length; i++) {
+        if(iMax < arr[i]){
+            iMax = arr[i];
+        }
+    }
+    return iMax
+}
+
+findMax(arr)
